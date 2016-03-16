65e502612a7b438da2ffd4167f92674eb09f9bf5
Ian Holmes <ihh@berkeley.edu>
2016-03-16 16:14:39 -0700

    Added some more ontology tests

--- test/ontology.js
+++ test/ontology.js
@@ -6,28 +6,47 @@ var Ontology = require('../ontology'),
 describe('Ontology', function() {
 
     var json = [
-        ["arachnid", "animal"],
-        ["mammal", "animal"],
-        ["spider", "arachnid"],
-        ["primate", "mammal"],
-        ["man", "primate"],
-        ["spiderman", "spider", "man"],
-        ["kingkong", "primate"]
+        ["arachnid", "animal"],  // 0
+        ["mammal", "animal"],  // 1
+        ["spider", "arachnid"],  // 2
+        ["primate", "mammal"],  // 3
+        ["man", "primate"],  // 4
+        ["spiderman", "spider", "man"],  // 5
+        ["kingkong", "primate"]  // 6
     ]
 
     var onto = new Ontology (json)
     var ontoJson = onto.toJSON()
     
     var jsonFull = json.slice()
-    jsonFull.push (["animal"])
+    jsonFull.push (["animal"])  // 7
 
     var ontoFull = new Ontology (jsonFull)
     var ontoJsonFull = ontoFull.toJSON()
 
     describe('#constructor', function() {
+        it('should parse explicitly declared terms', function() {
+            assert (ontoFull.termName.length == 8)
+        })
         it('should detect implicit terms', function() {
-            assert (onto.terms() == 8)
+            assert (onto.termName.length == 8)
             assert (onto.termName[7] == 'animal')
+            assert (onto.parents[7].length == 0)
+        })
+        it('should map term names to term indices', function() {
+            assert (ontoFull.termIndex['arachnid'] == 0)
+            assert (ontoFull.termIndex['spider'] == 2)
+            assert (ontoFull.termIndex['man'] == 4)
+            assert (ontoFull.termIndex['spiderman'] == 5)
+            assert (ontoFull.termIndex['animal'] == 7)
+        })
+        it('should map parent names to term indices', function() {
+            assert (ontoFull.parents[0].length == 1)
+            assert (ontoFull.parents[0][0] == 7)
+
+            assert (ontoFull.parents[5].length == 2)
+            assert (ontoFull.parents[5][0] == 2)
+            assert (ontoFull.parents[5][1] == 4)
         })
     })
 
