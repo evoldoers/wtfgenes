0ec9660be1fe74e3f9bedf95e532b2c6f04f3bfa
Ian Holmes <ihh@berkeley.edu>
2016-03-16 22:23:19 -0700

    gene-term associations

---
+++ assocs.js
@@ -0,0 +1,61 @@
+(function() {
+    var extend = require('util')._extend,
+        assert = require('assert')
+
+    function toJSON() {
+        var assocs = this
+        var gt = []
+        for (var g = 0; g < assocs.genes(); ++g)
+            assocs.termsByGene[g].forEach (function(t) {
+                gt.push ([assocs.geneName[g], assocs.ontology.termName[t]])
+            })
+        return gt
+    }
+    
+    function Assocs (ontology, geneTermList, wantTransitiveClosure) {
+        var assocs = this
+        extend (assocs,
+                { 'ontology': ontology,
+                  'geneName': [],
+                  'geneIndex': {},
+                  'genesByTerm': [],
+                  'termsByGene': [],
+                  'genes': function() { return this.geneName.length },
+                  'terms': function() { return this.ontology.terms() },
+                  'toJSON': toJSON
+                })
+
+        var gtIndexList = []
+        geneTermList.forEach (function(gt) {
+            var gene = gt[0], term = gt[1]
+            if (!(gene in assocs.geneIndex)) {
+                assocs.geneIndex[gene] = assocs.genes()
+                assocs.geneName.push (gene)
+            }
+            if (!(term in ontology.termIndex))
+                throw new Error ("Term " + term + " not found in the ontology")
+        })
+
+        assocs.genesByTerm = assocs.ontology.termName.map (function() { return [] })
+        assocs.termsByGene = assocs.geneName.map (function() { return [] })
+
+        var closure
+        if (wantTransitiveClosure)
+            closure = ontology.transitiveClosure()
+        else {
+            closure = []
+            for (var t = 0; t < ontology.terms(); ++t)
+                closure.push ([t])
+        }
+
+        geneTermList.forEach (function(gt) {
+            var gene = assocs.geneIndex[gt[0]], term = ontology.termIndex[gt[1]]
+            closure[term].forEach (function(t) {
+                assocs.termsByGene[gene].push (t)
+                assocs.genesByTerm[t].push (gene)
+            })
+        })
+    }
+
+    module.exports = Assocs
+}) ()
---
+++ test/assocs.js
@@ -0,0 +1,81 @@
+// for use with mocha test framework
+
+var Ontology = require('../ontology'),
+    Assocs = require('../assocs'),
+    assert = require('assert')
+
+describe('Assocs', function() {
+
+    var ontoJson = [
+        ["arachnid", "animal"],
+        ["mammal", "animal"],
+        ["spider", "arachnid"],
+        ["primate", "mammal"],
+        ["human", "primate"],
+        ["spiderhuman", "arachnid", "human", "mutant"],
+        ["gorilla", "primate"],
+        ["animal"],
+        ["mutant"]
+    ]
+
+    var onto = new Ontology (ontoJson)
+
+    var gt = [["peter-parker", "spiderhuman"],
+              ["may-parker", "spiderhuman"],
+              ["socrates", "human"],
+              ["charlotte", "spider"],
+              ["kingkong", "gorilla"],
+              ["kingkong", "mutant"]]
+
+    var gtTrans = [["peter-parker", "arachnid"],
+                   ["peter-parker", "mammal"],
+                   ["peter-parker", "primate"],
+                   ["peter-parker", "human"],
+                   ["peter-parker", "spiderhuman"],
+                   ["peter-parker", "animal"],
+                   ["peter-parker", "mutant"],
+                   ["may-parker", "arachnid"],
+                   ["may-parker", "mammal"],
+                   ["may-parker", "primate"],
+                   ["may-parker", "human"],
+                   ["may-parker", "spiderhuman"],
+                   ["may-parker", "animal"],
+                   ["may-parker", "mutant"],
+                   ["socrates", "mammal"],
+                   ["socrates", "primate"],
+                   ["socrates", "human"],
+                   ["socrates", "animal"],
+                   ["charlotte", "arachnid"],
+                   ["charlotte", "spider"],
+                   ["charlotte", "animal"],
+                   ["kingkong", "mammal"],
+                   ["kingkong", "primate"],
+                   ["kingkong", "gorilla"],
+                   ["kingkong", "animal"],
+                   ["kingkong", "mutant"]]
+
+    var assocs = new Assocs (onto, gt)
+    var transAssocs = new Assocs (onto, gt, true)
+    
+    describe('#constructor', function() {
+        it('should parse genes', function() {
+            assert.equal (assocs.genes(), 5)
+        })
+    })
+
+    describe('#toJSON', function() {
+        var assocsJson = assocs.toJSON()
+        it('should be idempotent with constructor', function() {
+            assert.deepEqual (assocsJson, gt)
+        })
+    })
+
+    describe('#constructor', function() {
+        var transAssocsJson = transAssocs.toJSON()
+        console.log (transAssocsJson)
+        it('should form transitive associations if mandated', function() {
+            assert.deepEqual (transAssocsJson, gtTrans)
+        })
+    })
+
+})
