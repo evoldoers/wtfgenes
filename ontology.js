baf1a532ff2e370344e2484d3aeea6dd9cbdb80f
Ian Holmes <ihh@berkeley.edu>
2016-03-16 17:01:44 -0700

    toposort ontology

--- ontology.js
+++ ontology.js
@@ -10,15 +10,50 @@
         return json
     }
 
-    module.exports = function (termParents) {
+    function toposort() {
+        // Kahn, Arthur B. (1962), "Topological sorting of large networks", Communications of the ACM 5 (11): 558–562, doi:10.1145/368996.369025
+        // https://en.wikipedia.org/wiki/Topological_sorting
+        var onto = this
+        var S = [], L = []
+        var children = onto.parents.map (function() { return [] })
+        var nParents = [], edges = 0
+        for (var c = 0; c < onto.terms(); ++c) {
+            onto.parents[c].forEach (function(p) {
+                children[p].push (c)
+                ++edges
+            })
+            nParents[c] = onto.parents[c].length
+            if (nParents[c] == 0)
+                S.push (c)
+        }
+        while (S.length > 0) {
+            var n = S.shift()
+            L.push (n)
+            children[n].forEach (function(m) {
+                --edges
+                if (--nParents[m] == 0)
+                    S.push (m)
+            })
+        }
+        if (edges > 0)
+            throw ("Ontology graph is not a DAG")
+
+        var json = onto.toJSON()
+        var toposortedJson = L.map (function(idx) { return json[idx] })
+
+        return new Ontology (toposortedJson)
+    }
+
+    function Ontology (termParents) {
         var onto = this
         extend (onto,
                 { 'termName': [],
                   'termIndex': {},
                   'parents': [],
                   'closure': [],
+                  'terms': function() { return this.termName.length },
                   'toJSON': toJSON,
-                  'terms': function() { return this.termName.length } })
+                  'toposort': toposort })
         var extTermParents = []
         termParents.forEach (function (tp) {
             extTermParents.push (tp)
@@ -39,4 +74,6 @@
             onto.parents[onto.termIndex[tp[0]]] = tp.slice([1]).map (function(n) { return onto.termIndex[n] })
         })
     }
+
+    module.exports = Ontology
 }) ()
--- test/ontology.js
+++ test/ontology.js
@@ -15,18 +15,18 @@ describe('Ontology', function() {
         ["kingkong", "primate"]  // 6
     ]
 
-    var onto = new Ontology (json)
-    var ontoJson = onto.toJSON()
-    
-    var jsonFull = json.slice()
-    jsonFull.push (["animal"])  // 7
+    var fullJson = json.slice()
+    fullJson.push (["animal"])  // 7
 
-    var ontoFull = new Ontology (jsonFull)
-    var ontoJsonFull = ontoFull.toJSON()
+    var topJson = [7,0,1,2,3,4,6,5]  // Kahn's sort ordering (NB kingkong now precedes spiderman)
+        .map (function(i) { return fullJson[i] })
+
+    var onto = new Ontology (json)
+    var fullOnto = new Ontology (fullJson)
 
     describe('#constructor', function() {
         it('should parse explicitly declared terms', function() {
-            assert (ontoFull.termName.length == 8)
+            assert (fullOnto.termName.length == 8)
         })
         it('should detect implicit terms', function() {
             assert (onto.termName.length == 8)
@@ -34,31 +34,47 @@ describe('Ontology', function() {
             assert (onto.parents[7].length == 0)
         })
         it('should map term names to term indices', function() {
-            assert (ontoFull.termIndex['arachnid'] == 0)
-            assert (ontoFull.termIndex['spider'] == 2)
-            assert (ontoFull.termIndex['man'] == 4)
-            assert (ontoFull.termIndex['spiderman'] == 5)
-            assert (ontoFull.termIndex['animal'] == 7)
+            assert (fullOnto.termIndex['arachnid'] == 0)
+            assert (fullOnto.termIndex['spider'] == 2)
+            assert (fullOnto.termIndex['man'] == 4)
+            assert (fullOnto.termIndex['spiderman'] == 5)
+            assert (fullOnto.termIndex['animal'] == 7)
         })
         it('should map parent names to term indices', function() {
-            assert (ontoFull.parents[0].length == 1)
-            assert (ontoFull.parents[0][0] == 7)
+            assert (fullOnto.parents[0].length == 1)
+            assert (fullOnto.parents[0][0] == 7)
 
-            assert (ontoFull.parents[5].length == 2)
-            assert (ontoFull.parents[5][0] == 2)
-            assert (ontoFull.parents[5][1] == 4)
+            assert (fullOnto.parents[5].length == 2)
+            assert (fullOnto.parents[5][0] == 2)
+            assert (fullOnto.parents[5][1] == 4)
         })
     })
 
     describe('#toJSON', function() {
+        var fullOntoJson = fullOnto.toJSON()
         it('should be idempotent when composed with constructor', function() {
-            assert (JSON.stringify(jsonFull) == JSON.stringify(ontoJsonFull))
+            assert (JSON.stringify(fullOntoJson) == JSON.stringify(fullJson))
         })
     })
 
     describe('#terms', function() {
         it('should yield the number of terms', function() {
-            assert (ontoFull.terms() == jsonFull.length)
+            assert (fullOnto.terms() == fullJson.length)
+        })
+    })
+
+    describe('#toposort', function() {
+        var topOnto = fullOnto.toposort()
+        it('should preserve the number of terms', function() {
+            assert (topOnto.terms() == fullOnto.terms())
+        })
+        it('should topologically sort the ontology graph', function() {
+            for (var i = 0; i < topOnto.terms(); ++i)
+                topOnto.parents[i].forEach (function(p) { assert (p < i) })
+        })
+        it('should yield Kahn\'s ordering', function() {
+            var topOntoJson = topOnto.toJSON()
+            assert (JSON.stringify(topOntoJson) == JSON.stringify(topJson))
         })
     })
 })
