3cbd3c8092e630c503f6a95e99a4faf1524510fe
Ian Holmes <ihh@berkeley.edu>
2016-03-17 17:24:13 -0700

    test/bernouilli.js

--- bernouilli.js
+++ bernouilli.js
@@ -4,8 +4,8 @@
 
     function update (bp, param) {
 	var val = bp._params[param]
-	bp._logYes[param] = log (val)
-	bp._logNo[param] = log (1 - val)
+	bp._logYes[param] = Math.log (val)
+	bp._logNo[param] = Math.log (1 - val)
     }
 
     function logLikelihood (params, counts) {
@@ -21,13 +21,13 @@
 
     function add (counts) {
 	assert (this.params === counts.params)
-	var c = extend ({}, this)
+	var c = new BernouilliCounts (this)
 	for (var param in counts.pos)
 	    if (counts.pos.hasOwnProperty (param))
-		c.pos[param] += counts.pos[param]
+		c.pos[param] = counts.pos[param] + (c.pos[param] || 0)
 	for (var param in counts.neg)
 	    if (counts.neg.hasOwnProperty (param))
-		c.neg[param] += counts.neg[param]
+		c.neg[param] = counts.neg[param] + (c.neg[param] || 0)
 	return c
     }
 
@@ -35,27 +35,28 @@
         var bc = this
 	extend (bc, {
 	    params: counts.params,
-	    pos: counts.pos || {},
-	    neg: counts.neg || {},
+	    pos: extend ({}, counts.pos),
+	    neg: extend ({}, counts.neg),
 	    logLikelihood: function(params) { return logLikelihood(params || this.params,this) },
 	    add: add,
-	    toJSON: function() { return { pos: pos, neg: neg } }
+	    toJSON: function() { return { pos: this.pos, neg: this.neg } }
 	})
     }
 
     function BernouilliParams (params) {
         var bp = this
 	extend (bp, {
-	    _params: params,
+	    _params: params || {},
 	    _logYes: {},
 	    _logNo: {},
-	    getParam: function(param) { return _params[param] },
-	    setParam: function(param,val) { _params[param] = val; update (bp, param) },
+	    params: function() { return Object.keys(this._params).sort() },
+	    getParam: function(param) { return this._params[param] },
+	    setParam: function(param,val) { this._params[param] = val; update (bp, param) },
 	    logLikelihood: function(count) { return logLikelihood(this,count) },
 	    toJSON: function() { return extend ({}, this._params) },
 	    newCounts: function(c) { return new BernouilliCounts (extend ({ params: this }, c)) }
 	})
-	Object.keys(bp.params).forEach (function(param) {
+	Object.keys(params).forEach (function(param) {
 	    update (bp, param)
 	})
     }
---
+++ test/bernouilli.js
@@ -0,0 +1,94 @@
+// for use with mocha test framework
+
+var Bernouilli = require('../bernouilli'),
+    assert = require('assert')
+
+describe('BernouilliParams', function() {
+
+    var json = { 'c': .5,
+		 'b': .25,
+		 'a': .125 }
+
+    var params = [ 'a', 'b', 'c' ]
+
+    var bern = new Bernouilli (json)
+
+    describe('#constructor', function() {
+        it('should parse correct number of params', function() {
+            assert.deepEqual (bern.params(), params)
+        })
+    })
+
+    describe('#getParam', function() {
+        it('should retrieve param values', function() {
+            assert.equal (bern.getParam('a'), .125)
+            assert.equal (bern.getParam('b'), .25)
+            assert.equal (bern.getParam('c'), .5)
+        })
+    })
+
+    describe('#setParam', function() {
+        it('should store param values', function() {
+            bern.setParam('a',.0625)
+            assert.equal (bern.getParam('a'), .0625)
+            bern.setParam('a',.125)
+            assert.equal (bern.getParam('a'), .125)
+        })
+    })
+
+    describe('#toJSON', function() {
+        it('should serialize and deserialize idempotently', function() {
+            assert.deepEqual (bern.toJSON(), json)
+        })
+    })
+
+    describe('#newCounts', function() {
+	var a3b1Json = {pos:{a:3},neg:{b:1}}
+	var a1c5Json = {pos:{a:1},neg:{c:5}}
+	var a4b1c5Json = {pos:{a:4},neg:{b:1,c:5}}
+	var zeroCounts = bern.newCounts()
+	var a3b1Counts = bern.newCounts(a3b1Json)
+	var a1c5Counts = bern.newCounts(a1c5Json)
+        it('should return empty counts by default', function() {
+            assert.deepEqual (zeroCounts.pos, {})
+            assert.deepEqual (zeroCounts.neg, {})
+        })
+        it('should accept optional counts argument', function() {
+            assert.equal (a3b1Counts.pos.a, 3)
+            assert.equal (a3b1Counts.neg.b, 1)
+        })
+
+	describe('BernouilliCounts', function() {
+	    describe('#toJSON', function() {
+		it('should serialize and deserialize idempotently', function() {
+		    assert.deepEqual (a3b1Counts.toJSON(), a3b1Json)
+		})
+	    })
+	    describe('#add', function() {
+		it('should add counts', function() {
+		    var a4b1c5Counts = a3b1Counts.add (a1c5Counts)
+		    assert.deepEqual (a4b1c5Counts.toJSON(), a4b1c5Json)
+		})
+		it('should not modify originals', function() {
+		    assert.deepEqual (a3b1Counts.toJSON(), a3b1Json)
+		    assert.deepEqual (a1c5Counts.toJSON(), a1c5Json)
+		})
+	    })
+	    describe('#logLikelihood', function() {
+		it('should compute log-likelihood', function() {
+		    assert.equal (a3b1Counts.logLikelihood(),
+				  3 * Math.log(bern.getParam('a')) + Math.log(1 - bern.getParam('b')))
+		})
+		it('should work after parameters changed', function() {
+		    bern.setParam('a',.66)
+		    bern.setParam('b',.33)
+		    assert.equal (bern.getParam('a'), .66)
+		    assert.equal (bern.getParam('b'), .33)
+		    assert.equal (a3b1Counts.logLikelihood(),
+				  3 * Math.log(bern.getParam('a')) + Math.log(1 - bern.getParam('b')))
+		})
+	    })
+	})
+    })
+
+})
