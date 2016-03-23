c57a24763e12e556a41a6c5fb4a35c056f68665b
Ian Holmes <ihh@berkeley.edu>
2016-03-23 15:38:53 -0700

    More tests using the monster ontology

--- test/data/monster/run.sh
+++ test/data/monster/run.sh
@@ -1,1 +1,1 @@
-./wtfgenes.js -o test/data/monster/ontology.json -a test/data/monster/assocs.json -g test/data/monster/genes.json -n 10000 -s 12345
+./wtfgenes.js -o test/data/monster/ontology.json -a test/data/monster/assocs.json -g test/data/monster/mutants.json -n 10000 -s 12345
