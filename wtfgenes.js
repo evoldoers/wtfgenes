dc7afe4444161b3cbf1b0df3943cd2300a369faf
Ian Holmes <ihh@berkeley.edu>
2016-03-24 19:01:05 -0700

    Changed priors again. Now P(term)=1/#terms, P(falsepos)=P(falseneg)=1/#genes

--- wtfgenes.js
+++ wtfgenes.js
@@ -14,12 +14,11 @@ var defaultSeed = 123456789
 var defaultSamplesPerTerm = 100
 
 // The default prior can be summarized as follows:
-// - P(term present) = 1/#terms, sample size #terms + 1
-// - P(false positive) = 1/#terms, sample size #terms + 1
-// - P(false negative) = 1/100, sample size 100
+// - P(term present) = 1/#terms, sample size (#terms + 1)
+// - P(false positive) = 1/#genes, sample size (#genes + 1)
+// - P(false negative) = 1/#genes, sample size (#genes + 1)
 var defaultTermPseudocount = 1
 var defaultFalseNegPseudocount = 1
-var defaultTruePosPseudocount = 99
 var defaultFalsePosPseudocount = 1
 
 var defaultMoveRate = { flip: 1, swap: 1, randomize: 0 }
@@ -33,9 +32,9 @@ var opt = getopt.create([
     ['T',  'terms=N'          , 'pseudocount: active terms (default='+defaultTermPseudocount+')'],
     ['t',  'absent-terms=N'   , 'pseudocount: inactive terms (default=#terms)'],
     ['N',  'false-negatives=N', 'pseudocount: false negatives (default='+defaultFalseNegPseudocount+')'],
-    ['p',  'true-positives=N' , 'pseudocount: true positives (default='+defaultTruePosPseudocount+')'],
+    ['p',  'true-positives=N' , 'pseudocount: true positives (default=#genes)'],
     ['P',  'false-positives=N', 'pseudocount: false positives (default='+defaultFalsePosPseudocount+')'],
-    ['n',  'true-negatives=N' , 'pseudocount: true negatives (default=#terms)'],
+    ['n',  'true-negatives=N' , 'pseudocount: true negatives (default=#genes)'],
     ['F',  'flip-rate=N'      , 'relative rate of term-toggling moves (default='+defaultMoveRate.flip+')'],
     ['S',  'swap-rate=N'      , 'relative rate of term-swapping moves (default='+defaultMoveRate.swap+')'],
     ['R',  'randomize-rate=N' , 'relative rate of term-randomizing moves (default='+defaultMoveRate.randomize+')'],
@@ -43,7 +42,7 @@ var opt = getopt.create([
     ['q' , 'quiet'            , 'don\'t log the usual things ("data", "progress")'],
     ['r' , 'rnd-seed=N'       , 'seed random number generator (default=' + defaultSeed + ')'],
     ['m' , 'simulate=N'       , 'instead of doing inference, simulate N gene sets'],
-    ['h' , 'help'             , 'display this help']
+    ['h' , 'help'             , 'display this help message']
 ])              // create Getopt instance
 .bindHelp()     // bind option 'help' to default action
 .parseSystem(); // parse command line
@@ -91,8 +90,8 @@ var prior = {
     },
     fail: {
 	t: parseInt(opt.options['absent-terms']) || assocs.relevantTerms().length,
-	fp: parseInt(opt.options['true-negatives']) || assocs.relevantTerms().length,
-	fn: parseInt(opt.options['true-positives']) || defaultTruePosPseudocount
+	fp: parseInt(opt.options['true-negatives']) || assocs.genes(),
+	fn: parseInt(opt.options['true-positives']) || assocs.genes()
     }
 }
 
