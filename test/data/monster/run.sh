f282832b871740974db52e0655622ec42c6266cd
Ian Holmes <ihh@berkeley.edu>
2016-03-28 13:47:57 -0700

    updated path to wtfgenes in test script

--- test/data/monster/run.sh
+++ test/data/monster/run.sh
@@ -1,1 +1,1 @@
-./wtfgenes.js -o test/data/monster/ontology.json -a test/data/monster/assocs.json -g test/data/monster/mutants.json -s 1000 -r 12345
+bin/wtfgenes.js -o test/data/monster/ontology.json -a test/data/monster/assocs.json -g test/data/monster/mutants.json -s 1000 -r 12345
