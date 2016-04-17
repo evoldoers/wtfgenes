afcdee45328f2e97d3479f25cf5a3e7366075781
Ian Holmes <ihh@berkeley.edu>
2016-04-17 10:21:46 -0700

    gaf2json can filter to an ontology

--- bin/gaf2json.js
+++ bin/gaf2json.js
@@ -3,12 +3,15 @@
 var fs = require('fs'),
     path = require('path'),
     getopt = require('node-getopt'),
-    gaf2json = require('../lib/converters').gaf2json
+    gaf2json = require('../lib/converters').gaf2json,
+    Ontology = require('../lib/ontology'),
+    Assocs = require('../lib/assocs')
 
 var opt = getopt.create([
     ['d' , 'database-id'      , 'use database ID, rather than gene symbol'],
     ['m' , 'merge-duplicates' , 'merge aliases in >1 set, instead of discarding'],
     ['a' , 'aliases=PATH'     , 'file of aliases (synonyms on same line)'],
+    ['o' , 'ontology=PATH'    , 'filter using (JSON-format) ontology file'],
     ['h' , 'help'             , 'display this help message']
 ])              // create Getopt instance
 .bindHelp()     // bind option 'help' to default action
@@ -33,7 +36,19 @@ var aliases
 if ('aliases' in opt.options)
     aliases = fs.readFileSync(opt.options.aliases).toString()
 
-console.log (JSON.stringify (gaf2json ({ gaf: text,
-                                         aliases: aliases,
-                                         mergeDuplicates: opt.options['merge-duplicates'],
-					 useDatabaseID: useDatabaseID })))
+var assocsJson = gaf2json ({ gaf: text,
+                             aliases: aliases,
+                             mergeDuplicates: opt.options['merge-duplicates'],
+			     useDatabaseID: useDatabaseID })
+
+if ('ontology' in opt.options) {
+    var ontology = new Ontology (JSON.parse (fs.readFileSync (opt.options.ontology)))
+    var assocs = new Assocs ({ idAliasTerm: assocsJson.idAliasTerm,
+                               ontology: ontology,
+                               ignoreMissingTerms: true,
+                               closure: false })
+
+    assocsJson = assocs.toJSON()
+}
+
+console.log (JSON.stringify (assocsJson))
